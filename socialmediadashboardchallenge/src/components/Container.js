4408ce939272abb9b08eb9ef95596ffa698fad67
Firouzfar <firouzfar.sebastien@gmail.com>
2022-05-06 21:18:59 +0200

    Put the container

---
+++ socialmediadashboardchallenge/src/components/Container.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import "../App.scss"
+
+export const Container = () => {
+    const [array, setArray] = useState()
+    return (
+        <div className="container">
+            <div className="item-container">
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Container
